9edd303e3fae1f4c68851db476b9cf8f921ab538
Arturo Aguilera <35408784+Artuga@users.noreply.github.com>
2020-01-15 14:41:58 -0600

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+class Usuario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
+    rol = models.ForeignKey(Rol, on_delete=models.PROTECT, blank=True, null=True)
+    userid = models.CharField(max_length=400,blank=True, null= True)
+    nombre = models.CharField(max_length=400, default="")
+    apellido = models.CharField(max_length=400, default="")
+    celular = models.CharField(max_length=400, default="")
+    activo = models.BooleanField(default=True)
+    creationdate = models.DateTimeField(default=timezone.now, editable=False)
+    def __str__(self):
+        return self.user.username + "--" + str(self.nombre)
---
+++ blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import *
+
+# Create your views here.
+
+def index(request):
+    return redirect('/blog/login/')
+
+
+def login_aux(request):
+    print(settings.STATIC_ROOT)
+    return redirect('/blog/login/')
+
+
+def login_page(request):
+    return render(request,
+                  'login/login.html', {'error': ''})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['user']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            if Usuario.objects.filter(user=user.id).exists():
+                rol = Usuario.objects.get(user=user.id)
+                request.session['userrol'] = rol.rol.nombre
+                return redirect('/blog/main/')
+            else:
+                return render(request,'login/login.html',{'error':'Error de inicio de sesión de usuario, el usuario no existe o las credenciales son incorrectas'})
+        else:
+            return render(request,'login/login.html', {'error': 'Error de inicio de sesión de usuario, el usuario no existe o las credenciales son incorrectas'})
+        
+    else:
+        return render(request,'login/login.html', {'error': 'You must use POST to login'})
+        
+
+def logout_user(request):
+    if request.user.is_authenticated():
+        return redirect('/blog/login')
+    else:
+        return redirect('/blog/main')
+
+def main(request):
+    return render(request,
+                  'main/main.html', {'error': ''})
+
