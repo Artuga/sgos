80f665689f497955b4b2088f81243148851bef99
Antonio Miravete <36893264+AntonioKG@users.noreply.github.com>
2020-01-15 22:57:36 -0600

    Adicion de modelos
    Se crearon los modelos Grado, Grupo, Alumno, Pago y Gasto. Se agregaron los modelos a admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,3 +5,8 @@ from .models import *
 
 admin.site.register(Usuario)
 admin.site.register(Rol)
+admin.site.register(Grado)
+admin.site.register(Grupo)
+admin.site.register(Alumno)
+admin.site.register(Pago)
+admin.site.register(Gasto)
